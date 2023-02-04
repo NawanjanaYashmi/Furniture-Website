45b81d3a5bad5ea96151ef7f558220740e5d1e85
IT21061134 <nawanjanayeshmi@gmail.com>
2023-02-04 12:57:24 +0530

    add image for testimonial.js

--- furniture-starter-main/src/components/Testimonial.js
+++ furniture-starter-main/src/components/Testimonial.js
@@ -1,7 +1,34 @@
 import React from 'react';
 
+//import components
+import TestimonialSlider from './TestimonialSlider';
+
+//import data
+import { testimonial } from '../data';
+
 const Testimonial = () => {
-  return <div>Testimonial</div>;
+  //destructure testimonial
+  const { image , title } = testimonial;
+  
+  return (
+    <section className='section'>
+      <div className='container mx-auto'>
+        <div className='flex flex-col lg:flex-row lg:gap-x-10 gap-y-2'>
+          <div>
+            {/*title*/}
+            <h2>{title}</h2>
+            {/*slider*/}
+            <TestimonialSlider />
+          </div>
+          {/*image*/}
+          <div className='order-1'>
+            <img src={image.type} all='' />
+          </div>
+        </div>
+
+      </div>
+    </section>
+  );
 };
 
 export default Testimonial;
